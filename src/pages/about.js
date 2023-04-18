9fe2f2439353d0cd64c0467e1aa77701ae15be29
Naymul Islam <naymulislam01855@gmail.com>
2023-04-18 17:52:21 +0600

    about us done v.1

--- src/pages/about.js
+++ src/pages/about.js
@@ -25,7 +25,7 @@ const About = () => {
    
       <VideoOne />
       <TeamSectionTwo />
-      <DiscoverSection ShowTitle={false} />
+      {/* <DiscoverSection ShowTitle={this} /> */}
       <FeatureSix />
       
       
