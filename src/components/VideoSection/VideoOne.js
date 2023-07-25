69bf3e9b9e71cb28edf416065122fe85799f7df4
Naymul Islam <naymulislam01855@gmail.com>
2023-07-25 13:03:27 +0600

    replaced agency to software company text

--- src/components/VideoSection/VideoOne.js
+++ src/components/VideoSection/VideoOne.js
@@ -35,7 +35,7 @@ const VideoOne = () => {
                   
                   </h2></center>
                   
-                  <h5 className="mt-5">Bugs Bee, a dedicated digital agency, is now among one of the leading ones currently operating globally. It offers a vast amount of digital products ensuring the use of up-to-date tools, optimized to be most efficient for its clients. From designing simple apps to creating whole robust Web Applications, rest assured, cause we got you covered! We have a bunch of specialized Programmers and IT Professionals improvised just to meet your needs!</h5>
+                  <h5 className="mt-5">Bugs Bee, a Software Company , is now among one of the leading ones currently operating globally. It offers a vast amount of digital products ensuring the use of up-to-date tools, optimized to be most efficient for its clients. From designing simple apps to creating whole robust Web Applications, rest assured, cause we got you covered! We have a bunch of specialized Programmers and IT Professionals improvised just to meet your needs!</h5>
                 </div>
               </Col>
               {/* <Col md={12} lg={4}>
